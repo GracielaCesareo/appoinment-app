a81a2d79829a9bdca12d3b8f97b6e3f7072a74c0
Graciela Cesareo <graziellac@Gracielas-MacBook-Air.local>
2020-05-16 17:53:01 -0500

    Validation for empty field in form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,65 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 
 const Form = () => {
+    // Create State for Appointments
+    const [appointment, updateAppointment]=useState({
+        pet: '',
+        owner:'',
+        date: '',
+        time: '',
+        details: ''
+    });
+
+    const [error, updateError] = useState(false)
+
+    // Function listened on user write
+    const updateState = (e) => {
+        updateAppointment({
+            ...appointment,
+            [e.target.name]: e.target.value
+        })
+        
+    }
+
+    // Get inputs values
+    const {pet, owner, date, time, details} = appointment;
+
+    // Submitting form
+    const submitAppointment = e => {
+        e.preventDefault();
+
+        // Validate input
+        if(pet.trim() === '' || owner.trim() === '' || date.trim() === '' || time.trim() === '' || details.trim() === ''){
+            updateError(true);
+            return
+        }
+
+        // Set ID
+
+        // Create Appointment
+
+        // Clean form
+        
+    }
+
     return (
         <Fragment>
             <h1>Book an appointment</h1>
 
-            <form action="">
+            {error ? <p className="alerta-error">All fields are required</p> : null}
+
+            <form
+            action=""
+            onSubmit={submitAppointment}
+            >
                 <label htmlFor="pet">Pet Name</label>
                 <input 
                 type="text"
                 name="pet"
                 className="u-full-width"
                 placeholder="Pet name"
+                onChange={updateState}
+                value={pet}
                 />
 
                 <label htmlFor="owner">Owner Name</label>
@@ -20,6 +68,8 @@ const Form = () => {
                 name="owner"
                 className="u-full-width"
                 placeholder="Owner Name"
+                onChange={updateState}
+                value={owner}
                 />
 
                 <label htmlFor="date">Appointment Date</label>
@@ -27,6 +77,8 @@ const Form = () => {
                 type="date"
                 name="date"
                 className="u-full-width"
+                onChange={updateState}
+                value={date}
                 />
 
                 <label htmlFor="date">Appointment Time</label>
@@ -34,12 +86,17 @@ const Form = () => {
                 type="time"
                 name="time"
                 className="u-full-width"
+                onChange={updateState}
+                value={time}
                 />
 
                 <label htmlFor="details">Details</label>
                 <textarea
                 className="u-full-width"
-                name="details">
+                name="details"
+                onChange={updateState}
+                value={details}
+                >
                 </textarea>
 
                 <button 
